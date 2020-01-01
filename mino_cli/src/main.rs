09126b06626ff72066ebe3994ed768f29b2f5e91
Shintaro Seki <s2pch.luck@gmail.com>
2020-01-01 20:51:18 +0900

    update

--- mino_cli/src/main.rs
+++ mino_cli/src/main.rs
@@ -3,10 +3,7 @@ extern crate mino_core;
 extern crate rand;
 extern crate termion;
 extern crate tui;
-use grid::IsEmpty;
-use mino_core::common::{
-    Cell, FallingPiece, Game, GameConfig, GameData, GameEvent, GameParams, Input, Playfield, TSpin,
-};
+use mino_core::common::{Game, GameConfig, GameData, GameEvent, GameParams, Input, Playfield};
 use mino_core::tetro::{Piece, PieceGrid, WorldRuleLogic};
 use rand::seq::SliceRandom;
 use std::collections::VecDeque;
@@ -16,78 +13,18 @@ use std::time;
 use termion::event::{Event, Key};
 use termion::raw::IntoRawMode;
 use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Direction, Layout, Rect};
+use tui::layout::{Constraint, Direction, Layout};
 use tui::style::{Color, Style};
 use tui::widgets::{Block, Paragraph, Text, Widget};
 use tui::Terminal;
 
-struct ViewDataBuilder {
-    ghost_piece: Option<FallingPiece<Piece>>,
-}
-
-impl ViewDataBuilder {
-    fn new(data: &GameData<Piece>) -> Self {
-        Self {
-            ghost_piece: if let Some(fp) = data.falling_piece {
-                let n = fp.droppable_rows(&data.playfield);
-                let mut gp = fp.clone();
-                gp.y -= n as i32;
-                Some(gp)
-            } else {
-                None
-            },
-        }
-    }
-
-    fn get_cell(&self, data: &GameData<Piece>, x: usize, y: usize) -> Cell<Piece> {
-        let pf = &data.playfield;
-        if let Some(fp) = data.falling_piece {
-            let x = x as i32 - fp.x;
-            let y = y as i32 - fp.y;
-            if fp.grid().is_valid_cell_index(x as usize, y as usize) {
-                let c = fp.grid().cell(x as usize, y as usize);
-                if !c.is_empty() {
-                    return c;
-                }
-            }
-        }
-        if let Some(gp) = self.ghost_piece {
-            let x = x as i32 - gp.x;
-            let y = y as i32 - gp.y;
-            if gp.grid().is_valid_cell_index(x as usize, y as usize) {
-                if let Cell::Block(p) = gp.grid().cell(x as usize, y as usize) {
-                    return Cell::Ghost(p);
-                }
-            }
-        }
-        pf.grid.cell(x, y)
-    }
-}
+mod renderer;
 
 fn generate_pieces() -> VecDeque<Piece> {
     let mut rng = rand::thread_rng();
     let mut ps = Piece::slice().clone();
     ps.shuffle(&mut rng);
     ps.to_vec().into()
-}
-
-fn format_cell(cell: Cell<Piece>) -> (String, Color) {
-    match cell {
-        Cell::Block(p) => (
-            format!("{}", p),
-            match p {
-                Piece::I => Color::Cyan,
-                Piece::O => Color::Yellow,
-                Piece::T => Color::Magenta,
-                Piece::J => Color::Blue,
-                Piece::L => Color::LightRed,
-                Piece::S => Color::Green,
-                Piece::Z => Color::Red,
-            },
-        ),
-        Cell::Ghost(p) => (format!("{}", p), Color::DarkGray),
-        _ => (" ".into(), Color::Black),
-    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -124,7 +61,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stdin = termion::async_stdin().bytes();
 
     // lines, tspin, remaining frames
-    let mut line_clear = (0, TSpin::None, 0);
+    let mut line_clear = (renderer::LineClearInfo::default(), 0);
 
     loop {
         let frame_started_at = time::Instant::now();
@@ -161,7 +98,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         for event in &game.data().events {
             match event {
                 GameEvent::LineCleared(n, t) => {
-                    line_clear = (*n, *t, 60 * 2);
+                    line_clear.0.n = *n;
+                    line_clear.0.tspin = *t;
+                    line_clear.1 = 60 * 2;
                     break;
                 }
                 _ => {}
@@ -169,75 +108,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         terminal.draw(|mut f| {
-            let data = game.data();
             let size = f.size();
-            Block::default()
-                .style(Style::default().bg(Color::Black))
-                .render(&mut f, size);
             let chunks = Layout::default()
                 .direction(Direction::Horizontal)
                 .constraints([Constraint::Length(10), Constraint::Percentage(90)].as_ref())
                 .split(size);
+            Block::default()
+                .style(Style::default().bg(Color::Black))
+                .render(&mut f, size);
             // Left pane
-            let mut top = 0;
-            {
-                let mut text = vec![Text::raw("HOLD:")];
-                let t = if let Some(p) = data.hold_piece {
-                    format_cell(Cell::Block(p))
+            renderer::render(
+                &mut f,
+                game.data(),
+                if line_clear.1 > 0 {
+                    Some(&line_clear.0)
                 } else {
-                    ("     ".into(), Color::Black)
-                };
-                text.push(Text::styled(t.0, Style::default().fg(Color::Black).bg(t.1)));
-                Paragraph::new(text.iter()).render(&mut f, Rect::new(0, top, 10, 1));
-                top += 1;
-            }
-            {
-                let mut text = vec![Text::raw("NEXT:")];
-                let mut ts: Vec<(String, Color)> = Vec::new();
-                for i in 0..5 {
-                    let t = if let Some(p) = data.next_pieces.get(i) {
-                        format_cell(Cell::Block(*p))
-                    } else {
-                        ("     ".into(), Color::Black)
-                    };
-                    ts.push(t);
-                }
-                for t in ts {
-                    text.push(Text::styled(t.0, Style::default().fg(Color::Black).bg(t.1)));
-                }
-                Paragraph::new(text.iter()).render(&mut f, Rect::new(0, top, 10, 1));
-                top += 1;
-            }
-            {
-                let pf = &data.playfield;
-                let vdb = ViewDataBuilder::new(&data);
-                for y in 0..pf.visible_rows {
-                    for x in 0..pf.grid.num_cols() {
-                        let t = format_cell(vdb.get_cell(&data, x, y));
-                        let text = [Text::styled(t.0, Style::default().fg(Color::Black).bg(t.1))];
-                        Paragraph::new(text.iter()).render(
-                            &mut f,
-                            Rect::new(x as u16, top + (pf.visible_rows - 1 - y) as u16, 1, 1),
-                        );
-                    }
-                }
-                top += 20;
-            }
-            {
-                let text = [Text::raw("0123456789")];
-                Paragraph::new(text.iter()).render(&mut f, Rect::new(0, top, 10, 1));
-                top += 1;
-            }
-            if line_clear.2 > 0 {
-                line_clear.2 -= 1;
-                let t = match line_clear.1 {
-                    TSpin::None => format!("L{}", line_clear.0),
-                    TSpin::Mini => format!("TSM{}", line_clear.0),
-                    TSpin::Normal => format!("TS{}", line_clear.0),
-                };
-                let text = [Text::raw(&t)];
-                Paragraph::new(text.iter()).render(&mut f, Rect::new(0, top, 10, 1));
-            }
+                    None
+                },
+                (0, 0),
+            );
             // Right pane
             {
                 let text = [Text::raw(format!("{:?}", game))];
---
+++ mino_cli/src/renderer.rs
@@ -0,0 +1,147 @@
+use grid::IsEmpty;
+use mino_core::common::{Cell, FallingPiece, GameData, TSpin};
+use mino_core::tetro::Piece;
+use tui::layout::Rect;
+use tui::style::{Color, Style};
+use tui::widgets::{Paragraph, Text, Widget};
+
+struct ViewDataBuilder {
+    ghost_piece: Option<FallingPiece<Piece>>,
+}
+
+impl ViewDataBuilder {
+    fn new(data: &GameData<Piece>) -> Self {
+        Self {
+            ghost_piece: if let Some(fp) = data.falling_piece {
+                let n = fp.droppable_rows(&data.playfield);
+                let mut gp = fp.clone();
+                gp.y -= n as i32;
+                Some(gp)
+            } else {
+                None
+            },
+        }
+    }
+
+    fn get_cell(&self, data: &GameData<Piece>, x: usize, y: usize) -> Cell<Piece> {
+        let pf = &data.playfield;
+        if let Some(fp) = data.falling_piece {
+            let x = x as i32 - fp.x;
+            let y = y as i32 - fp.y;
+            if fp.grid().is_valid_cell_index(x as usize, y as usize) {
+                let c = fp.grid().cell(x as usize, y as usize);
+                if !c.is_empty() {
+                    return c;
+                }
+            }
+        }
+        if let Some(gp) = self.ghost_piece {
+            let x = x as i32 - gp.x;
+            let y = y as i32 - gp.y;
+            if gp.grid().is_valid_cell_index(x as usize, y as usize) {
+                if let Cell::Block(p) = gp.grid().cell(x as usize, y as usize) {
+                    return Cell::Ghost(p);
+                }
+            }
+        }
+        pf.grid.cell(x, y)
+    }
+}
+
+fn format_cell(cell: Cell<Piece>) -> (String, Color) {
+    match cell {
+        Cell::Block(p) => (
+            format!("{}", p),
+            match p {
+                Piece::I => Color::Cyan,
+                Piece::O => Color::Yellow,
+                Piece::T => Color::Magenta,
+                Piece::J => Color::Blue,
+                Piece::L => Color::LightRed,
+                Piece::S => Color::Green,
+                Piece::Z => Color::Red,
+            },
+        ),
+        Cell::Ghost(p) => (format!("{}", p), Color::DarkGray),
+        _ => (" ".into(), Color::Black),
+    }
+}
+
+#[derive(Default)]
+pub struct LineClearInfo {
+    pub n: usize,
+    pub tspin: TSpin,
+}
+
+pub fn render<B>(
+    f: &mut tui::Frame<B>,
+    data: &GameData<Piece>,
+    line_clear_info: Option<&LineClearInfo>,
+    pos: (u16, u16),
+) where
+    B: tui::backend::Backend,
+{
+    let mut top = pos.1;
+    {
+        let mut text = vec![Text::raw("HOLD:")];
+        let t = if let Some(p) = data.hold_piece {
+            format_cell(Cell::Block(p))
+        } else {
+            ("     ".into(), Color::Black)
+        };
+        text.push(Text::styled(t.0, Style::default().fg(Color::Black).bg(t.1)));
+        Paragraph::new(text.iter()).render(f, Rect::new(pos.0, top, 10, 1));
+        top += 1;
+    }
+    {
+        let mut text = vec![Text::raw("NEXT:")];
+        let mut ts: Vec<(String, Color)> = Vec::new();
+        for i in 0..5 {
+            let t = if let Some(p) = data.next_pieces.get(i) {
+                format_cell(Cell::Block(*p))
+            } else {
+                ("     ".into(), Color::Black)
+            };
+            ts.push(t);
+        }
+        for t in ts {
+            text.push(Text::styled(t.0, Style::default().fg(Color::Black).bg(t.1)));
+        }
+        Paragraph::new(text.iter()).render(f, Rect::new(pos.0, top, 10, 1));
+        top += 1;
+    }
+    {
+        let pf = &data.playfield;
+        let vdb = ViewDataBuilder::new(&data);
+        for y in 0..pf.visible_rows {
+            for x in 0..pf.grid.num_cols() {
+                let t = format_cell(vdb.get_cell(&data, x, y));
+                let text = [Text::styled(t.0, Style::default().fg(Color::Black).bg(t.1))];
+                Paragraph::new(text.iter()).render(
+                    f,
+                    Rect::new(
+                        pos.0 + x as u16,
+                        top + (pf.visible_rows - 1 - y) as u16,
+                        1,
+                        1,
+                    ),
+                );
+            }
+        }
+        top += 20;
+    }
+    {
+        let text = [Text::raw("==========")];
+        Paragraph::new(text.iter()).render(f, Rect::new(0, top, 10, 1));
+        top += 1;
+    }
+    if let Some(info) = line_clear_info {
+        let t = match info.tspin {
+            TSpin::None => format!("{} Lines!", info.n),
+            TSpin::Mini => format!("TSM{}!", "ZSTD".chars().nth(info.n).unwrap()),
+            TSpin::Normal => format!("TS{}!", "ZSTD".chars().nth(info.n).unwrap()),
+        };
+        let text = [Text::raw(&t)];
+        Paragraph::new(text.iter()).render(f, Rect::new(0, top, 10, 1));
+    }
+}
